bc13782331b61eb14f1520150a0d9e20a4713401
Dylan Dorn <ddorn343@gmail.com>
2020-08-07 13:35:51 -0700

    Activity 2

---
+++ Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> userModels = new List<UserModel>();
+            UserModel user1 = new UserModel("Bob", "test1@test.com", "(123)-456-7890");
+            UserModel user2 = new UserModel("John", "test2@test.com", "(123)-182-0952");
+            UserModel user3 = new UserModel("Dave", "test3@test.com", "(123)-923-0523");
+            userModels.Add(user1);
+            userModels.Add(user2);
+            userModels.Add(user3);
+
+            return View("Test", userModels);
+        }
+    }
+}
